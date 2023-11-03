c3844387b8d0148a5fecac32293e5df13ef5f318
Tёma Dot <amettod@gmail.com>
2023-11-03 09:07:09 +0300

    add theme

---
+++ theme/theme.go
@@ -0,0 +1,274 @@
+package theme
+
+import "sort"
+
+type Theme interface {
+	// Append theme.
+	Append(name string, background RGB, foreground RGB, alive []RGB, dead []RGB)
+	// Background color.
+	Background() (uint8, uint8, uint8)
+	// Foreground color.
+	Foreground() (uint8, uint8, uint8)
+	// Color return depending on cycle.
+	Color(cycle int) (uint8, uint8, uint8)
+	// Name theme.
+	Name() string
+	// Next theme.
+	Next()
+}
+
+type RGB struct {
+	r uint8
+	g uint8
+	b uint8
+}
+
+func NewRGB(r, g, b uint8) RGB {
+	return RGB{
+		r: r,
+		g: g,
+		b: b,
+	}
+}
+
+func (c RGB) color() (uint8, uint8, uint8) {
+	return c.r, c.g, c.b
+}
+
+type theme struct {
+	name       string
+	background RGB
+	foreground RGB
+	alive      []RGB
+	dead       []RGB
+}
+
+type themes struct {
+	current int
+	store   []theme
+}
+
+func New() Theme {
+	t := &themes{
+		current: 0,
+		store: []theme{
+			{
+				name:       "color16",
+				background: NewRGB(255, 255, 255),
+				foreground: NewRGB(0, 0, 0),
+				alive: []RGB{
+					NewRGB(0, 0, 0),
+					NewRGB(0, 0, 95),
+					NewRGB(0, 0, 135),
+					NewRGB(0, 0, 175),
+					NewRGB(0, 0, 215),
+					NewRGB(0, 0, 255),
+				},
+				dead: []RGB{
+					NewRGB(125, 255, 255),
+					NewRGB(135, 255, 255),
+					NewRGB(145, 255, 255),
+					NewRGB(165, 255, 255),
+					NewRGB(175, 255, 255),
+					NewRGB(185, 255, 255),
+					NewRGB(195, 255, 255),
+					NewRGB(205, 255, 255),
+					NewRGB(215, 255, 255),
+				},
+			},
+			{
+				name:       "orangeAndRed",
+				background: NewRGB(255, 255, 255),
+				foreground: NewRGB(0, 0, 0),
+				alive: []RGB{
+					NewRGB(255, 135, 0),
+					NewRGB(255, 120, 0),
+					NewRGB(255, 105, 0),
+					NewRGB(255, 90, 0),
+					NewRGB(255, 75, 0),
+					NewRGB(255, 60, 0),
+					NewRGB(255, 45, 0),
+					NewRGB(255, 30, 0),
+					NewRGB(255, 15, 0),
+					NewRGB(255, 0, 0),
+				},
+				dead: []RGB{
+					NewRGB(255, 155, 155),
+					NewRGB(255, 165, 165),
+					NewRGB(255, 175, 175),
+					NewRGB(255, 185, 185),
+					NewRGB(255, 195, 195),
+					NewRGB(255, 205, 205),
+					NewRGB(255, 215, 215),
+					NewRGB(255, 225, 225),
+					NewRGB(255, 235, 235),
+					NewRGB(255, 245, 245),
+				},
+			},
+			{
+				name:       "whiteAndBlack",
+				background: NewRGB(255, 255, 255),
+				foreground: NewRGB(0, 0, 0),
+				alive: []RGB{
+					NewRGB(0, 0, 0),
+				},
+				dead: []RGB{
+					NewRGB(255, 255, 255),
+				},
+			},
+			{
+				name:       "blackAndWhite",
+				background: NewRGB(0, 0, 0),
+				foreground: NewRGB(255, 255, 255),
+				alive: []RGB{
+					NewRGB(255, 255, 255),
+				},
+				dead: []RGB{
+					NewRGB(0, 0, 0),
+				},
+			},
+			{
+				name:       "ocean",
+				background: NewRGB(0, 0, 130),
+				foreground: NewRGB(255, 255, 255),
+				alive: []RGB{
+					NewRGB(75, 75, 255),
+					NewRGB(85, 85, 255),
+					NewRGB(95, 95, 255),
+					NewRGB(105, 105, 255),
+					NewRGB(115, 115, 255),
+					NewRGB(125, 125, 255),
+					NewRGB(135, 135, 255),
+					NewRGB(145, 145, 255),
+					NewRGB(255, 255, 255),
+				},
+				dead: []RGB{
+					NewRGB(0, 0, 70),
+					NewRGB(0, 0, 80),
+					NewRGB(0, 0, 90),
+					NewRGB(0, 0, 100),
+					NewRGB(0, 0, 110),
+					NewRGB(0, 0, 120),
+				},
+			},
+			{
+				name:       "fire",
+				background: NewRGB(130, 0, 0),
+				foreground: NewRGB(255, 255, 0),
+				alive: []RGB{
+					NewRGB(255, 0, 0),
+					NewRGB(255, 25, 0),
+					NewRGB(255, 50, 0),
+					NewRGB(255, 75, 0),
+					NewRGB(255, 100, 0),
+					NewRGB(255, 210, 0),
+					NewRGB(255, 220, 0),
+					NewRGB(255, 230, 0),
+					NewRGB(255, 245, 0),
+					NewRGB(255, 255, 0),
+				},
+				dead: []RGB{
+					NewRGB(70, 0, 0),
+					NewRGB(80, 0, 0),
+					NewRGB(90, 0, 0),
+					NewRGB(100, 0, 0),
+					NewRGB(110, 0, 0),
+					NewRGB(120, 0, 0),
+				},
+			},
+			{
+				name:       "matrix",
+				background: NewRGB(0, 0, 0),
+				foreground: NewRGB(0, 255, 0),
+				alive: []RGB{
+					NewRGB(0, 205, 0),
+					NewRGB(0, 215, 0),
+					NewRGB(0, 225, 0),
+					NewRGB(0, 235, 0),
+					NewRGB(0, 245, 0),
+					NewRGB(0, 255, 0),
+				},
+				dead: []RGB{
+					NewRGB(0, 70, 0),
+					NewRGB(0, 60, 0),
+					NewRGB(0, 50, 0),
+					NewRGB(0, 40, 0),
+					NewRGB(0, 30, 0),
+					NewRGB(0, 20, 0),
+					NewRGB(0, 10, 0),
+				},
+			},
+		},
+	}
+	t.sort()
+	return t
+}
+
+func (t *themes) theme() theme {
+	return t.store[t.current]
+}
+
+func (t *themes) sort() {
+	sort.Slice(t.store, func(i, j int) bool {
+		return t.store[i].name < t.store[j].name
+	})
+}
+
+func (t *themes) alive(cycle int) (uint8, uint8, uint8) {
+	c := cycle - 1
+	l := len(t.theme().alive)
+	if c < l {
+		return t.theme().alive[c].color()
+	}
+	return t.theme().alive[l-1].color()
+}
+
+func (t *themes) dead(cycle int) (uint8, uint8, uint8) {
+	c := cycle*-1 - 1
+	l := len(t.theme().dead)
+	if c < l {
+		return t.theme().dead[c].color()
+	}
+	return t.theme().background.color()
+}
+
+func (t *themes) Append(name string, background RGB, foreground RGB, alive []RGB, dead []RGB) {
+	t.store = append(t.store, theme{
+		name:       name,
+		background: background,
+		foreground: foreground,
+		alive:      alive,
+		dead:       dead,
+	})
+	t.sort()
+}
+
+func (t *themes) Background() (uint8, uint8, uint8) {
+	return t.theme().background.color()
+}
+
+func (t *themes) Foreground() (uint8, uint8, uint8) {
+	return t.theme().foreground.color()
+}
+
+func (t *themes) Color(cycle int) (uint8, uint8, uint8) {
+	switch {
+	case cycle > 0:
+		return t.alive(cycle)
+	case cycle < 0:
+		return t.dead(cycle)
+	default:
+		return t.Background()
+	}
+}
+
+func (t *themes) Name() string {
+	return t.theme().name
+}
+
+func (t *themes) Next() {
+	t.current++
+	if t.current > len(t.store)-1 {
+		t.current = 0
+	}
+}
